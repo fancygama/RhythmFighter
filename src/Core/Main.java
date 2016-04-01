863eee105e02e01081e6e6e6bbab975fdfed6972
Rick Carl <crickc2@hotmail.com>
2016-04-01 18:04:56 -0400

    Fuck that get money! Also took away the unnecessary constructor

--- src/Core/Main.java
+++ src/Core/Main.java
@@ -2,13 +2,9 @@ package Core;
 
 public class Main {
 
-	public Main() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
 	}
 
 }
