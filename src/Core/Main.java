dd0e65e74e776693d47adf4b5422d47013934249
Rick Carl <crickc2@hotmail.com>
2016-04-02 15:09:25 -0400

    Lots of new stuff, GamePanel still is buggy with displaying notes

--- src/Core/Main.java
+++ src/Core/Main.java
@@ -16,8 +16,7 @@ public class Main extends Thread{
 	public int frameWidth;
 	public int frameHeight;
 	//the game's song stuff. The audio player and the current position of the song.
-	private SongPlayer song;
-	private SongPlayer tutorialSong;
+	private RhythmTimer timer;
 	private long songPos;
 	//the classes containing the information of each player
 	private Player player1;
@@ -32,17 +31,18 @@ public class Main extends Thread{
 	public long p2LastHitOffset; //the amt player2's last hit was off the mark in ms
 	
 	public ArrayList<Long> beatsInSong;
+	private int currBeat = 0;
 	
 	//used by InListener to establish what should happen when a key is hit.
 	public void playerAttacked(int player, int move){
 		if (player == 1){
 			p1CanAtk = false;	//the player can no longer attack
 			player1.setLastMove(move);
-			p1LastHitOffset = Math.abs(SongPlayer.notesInSong.get(0) - song.getSongPos());
+			p1LastHitOffset = Math.abs(SongPlayer.notesInSong.get(0) - timer.getSongPos());
 		} else {
 			p2CanAtk = false;
 			player2.setLastMove(move);
-			p2LastHitOffset = Math.abs(SongPlayer.notesInSong.get(0) - song.getSongPos());
+			p2LastHitOffset = Math.abs(SongPlayer.notesInSong.get(0) - timer.getSongPos());
 		}
 	}
 	
@@ -50,7 +50,8 @@ public class Main extends Thread{
 	public void startUp(){	//the startup process for the game
 		
 		//set up the audio stuff
-		song = new SongPlayer("src/Resources/Song.wav");
+		timer = new RhythmTimer(new SongPlayer("src/Resources/Song.wav"));
+		SongPlayer.initNotesInSong();
 		beatsInSong = SongPlayer.notesInSong;
 		
 		//set up the players
@@ -83,7 +84,8 @@ public class Main extends Thread{
 	public void startUpTut(){
 		
 		//set up the audio stuff
-				song = new SongPlayer("src/Resources/Tutorial.wav");
+				timer = new RhythmTimer(new SongPlayer("src/Resources/Tutorial.wav"));
+				SongPlayer.initNotesInTut();
 				beatsInSong = SongPlayer.notesInTutorial;
 				tutorialOn = true;
 				
@@ -101,7 +103,8 @@ public class Main extends Thread{
 				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				frame.setPreferredSize(new Dimension(frameWidth,frameHeight));
 				//add in the panel to display everything
-				GamePanel panel = new GamePanel(frameWidth, frameHeight);
+			
+				panel = new GamePanel(frameWidth, frameHeight);
 				frame.add(panel);
 				frame.addKeyListener(new InListener(this));
 				panel.drawNoteLanes();
@@ -116,33 +119,35 @@ public class Main extends Thread{
 	
 	public void loop(){
 		
-		if (tutorialOn)
-			songPos = tutorialSong.getSongPos();
-		else
-		songPos = song.getSongPos();
-		
+		songPos = timer.getSongPos();
 			//This should be implemented once the array is set up
-		if (beatsInSong.size() != 0){
-
 		
-			  if (songPos - 100 <= beatsInSong.get(0) && songPos + 100 >= beatsInSong.get(0)){	//if the song is within +/- 100 ms of the next note
+		if (beatsInSong.size() != 0){
+		
+			  if (songPos - 100 <= beatsInSong.get(currBeat) && songPos + 100 >= beatsInSong.get(currBeat)){	//if the song is within +/- 100 ms of the next note
 			  		p1CanAtk = true;	//the players can use moves
 			  		p2CanAtk = true;
-			  
-			  		if (songPos - 10 <= beatsInSong.get(0) && songPos + 10 >= beatsInSong.get(0)){	//if the song is within +/- 10 ms of the next note
+			  		if (songPos - 10 <= beatsInSong.get(currBeat) && songPos + 10 >= beatsInSong.get(currBeat)){	//if the song is within +/- 10 ms of the next note
 			  			//A beat is happening now!
+			  			System.out.println(" Beat happened");
+			  			currBeat++;
+			  			if (panel.getNotesOnScreen().size() != 0){
 			  			panel.getNotesOnScreen().remove(0);
-			  			beatsInSong.remove(0);	//remove that note from the arrays, as it just happened.
+			  			//beatsInSong.remove(0);	//remove that note from the arrays, as it just happened.
+			  			}
 			  		}
 			  }
 			  
-			
 			//	This should also be implemented once the array is set up
-			  if (songPos >= beatsInSong.get(0) - 1000){	//adds a new beat to the panel if it's time
-			  		panel.getNotesOnScreen().add(frameWidth/2);
+			  if (songPos + 10 >= beatsInSong.get(currBeat + 1) - 1000 && songPos - 10 <= beatsInSong.get(currBeat + 1) - 1000){	//adds a new beat to the panel if it's time
+			  		//panel.getNotesOnScreen().add(frameWidth/2);
+				  	panel.getNotesOnScreen().add(beatsInSong.get(currBeat + 1));
+			  		System.out.println("added");
+					System.out.println("SongPos: " + songPos);
+					System.out.println("Current: " + beatsInSong.get(currBeat));
 			  			//or something like that
 			  	}
-		} else if (song.getSongPos() >= song.getSongLen()){
+		} else if (songPos >= timer.getSongLen()){
 			gameDone = true;
 			return;
 		}
@@ -152,7 +157,7 @@ public class Main extends Thread{
 		  panel.reset();
 		  //panel.drawBackground();		//for once we have a background
 		  panel.drawNoteLanes();
-		  panel.updateNotes();
+		  panel.updateNotes(songPos);
 		  //and then animation updates etc etc
 		
 		
@@ -162,16 +167,16 @@ public class Main extends Thread{
 	@Override
 	public void run(){
 		
-		startUp();
+		startUpTut();
 		
-		song.startSong();
+		timer.start();
+		System.out.println("Start time: " + timer.getSongPos());
 		while(!gameDone){
 			loop();
 			
 			try {
-				Main.sleep(30);	//an arbitrary number of ms between each iteration of loop. might cause lag...
+				Main.sleep(10);	//an arbitrary number of ms between each iteration of loop. might cause lag...
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
