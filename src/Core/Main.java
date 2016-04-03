5050ace1f72e297b9e8467abff96874e646b34cc
Rick Carl <crickc2@hotmail.com>
2016-04-03 01:58:52 -0400

    Fixed Main Stuff

--- src/Core/Main.java
+++ src/Core/Main.java
@@ -3,10 +3,16 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 
+import javax.imageio.ImageIO;
 import javax.sound.sampled.Clip;
 import javax.swing.*;
 
@@ -172,11 +178,38 @@ public void startUp(){	//the startup process for the game
 				
 		panel.removeAll();
 		panel.reset();
-		panel.drawNoteLanes();
+		backgroundLayer.drawNoteLanes();
+		
+		BufferedImage startButtonImage = new BufferedImage(frameWidth/4, frameHeight/6, BufferedImage.TYPE_INT_ARGB);
+		BufferedImage startButtonDepressed = new BufferedImage(frameWidth/4, frameHeight/6, BufferedImage.TYPE_INT_ARGB);
+		
+		try {
+			startButtonImage.createGraphics().drawImage(ImageIO.read(new File("src/resources/start.png")), 0, 0, frameWidth/4, frameHeight/6, null);
+			startButtonDepressed.createGraphics().drawImage(ImageIO.read(new File("src/resources/startpush.png")), 0, 0, frameWidth/4, frameHeight/6, null);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		JButton startButton = new JButton();
+		startButton.setOpaque(false);
+		startButton.setFocusable(false);
+		startButton.setContentAreaFilled(false);
+		startButton.setBorderPainted(false);
+		startButton.setBounds(frameWidth/2 - frameWidth/8, frameHeight/2, frameWidth/4, frameHeight/6);
+		startButton.setIcon(new ImageIcon(startButtonImage));
+		startButton.setPressedIcon(new ImageIcon(startButtonDepressed));
+		startButton.setOpaque(false);
+		startButton.addActionListener(new StartListener());
+
+		panel.add(startButton);
+		panel.repaint();
+		
 		//load it up!
 		frame.validate();
 		frame.setVisible(true);
 		frame.pack();
+		
 		
 		
 	}
@@ -199,7 +232,7 @@ public void startUp(){	//the startup process for the game
 				frame.addKeyListener(new InListener(this));
 				panel.removeAll();
 				panel.reset();
-				panel.drawNoteLanes();
+				backgroundLayer.drawNoteLanes();
 				//load it up!
 				frame.validate();
 				frame.setVisible(true);
@@ -379,21 +412,12 @@ public void startUp(){	//the startup process for the game
 		p2Color = Color.white;
 		curAnimProg = 0;
 		}
-	  panel.drawNoteLanes();
+	  //panel.drawNoteLanes();
 	  panel.drawScores();
 	  panel.updateNotes(songPos);
 	  //and then animation updates etc etc
 	  
 	  //System.out.println("This iteration started at " + songPos + " and ended at " + timer.getSongPos());
-		
-	}
-
-	
-	public void menuLoop(){	//loop for the menu.
-		
-	}
-	
-	public void resultsLoop(){
 		
 	}
 	
@@ -402,7 +426,7 @@ public void startUp(){	//the startup process for the game
 	public void run(){
 		
 		startUpMenu();
-		Timer menuTimer = new Timer(16, new MenuListener(this));
+		Timer menuTimer = new Timer(16, null);
 		menuTimer.setInitialDelay(0);
 		menuTimer.start();
 		
@@ -430,6 +454,14 @@ public void startUp(){	//the startup process for the game
 				}
 			}
 			startUp();
+			gamePhase = 0;
+			while (gamePhase == 0){
+				try {
+					Main.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 		
 		Timer newTimer = new Timer(16, new RhythmListener(this));
@@ -448,6 +480,18 @@ public void startUp(){	//the startup process for the game
 		
 		Main main = new Main();
 		main.start();
+		
+	}
+	
+	public class StartListener implements ActionListener{
+		
+		
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			// TODO Auto-generated method stub
+			gamePhase = 1;
+			panel.removeAll();
+		}
 		
 	}
 
